223c0eaba40ebd50507d39701013789db2267a8b
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-03-29 23:13:24 -0500

    Conslidate config files

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ set :port, 9494
 configure :development do
     enable :unsecure
     
-    db = Config.load_db("development")    
+    db = Config.load_config("development")    
     DataMapper::Logger.new($stdout, :debug)
     DataMapper.setup(
         :default,
@@ -53,7 +53,7 @@ configure :development do
 end
 
 configure :production do
-    db = Config.load_db("production")
+    db = Config.load_config("production")
     DataMapper.setup(
         :default,
         "mysql://" + db["user"] + ":" + db["password"] + "@" + db["hostname"]+ "/" + db["name"]
